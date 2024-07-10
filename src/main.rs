50072ea53f968b987db7d589aa9a5a6542951966
DiscRoomFan36 <fletcherhogarth@gmail.com>
2024-07-10 13:31:54 +1000

    grid hander. next is to make a new panel to consolodate, and handle over file dialog

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use file_dialog::*;
 
 use std::fs;
 use std::io::{Read, Write};
-use std::path::{Component, Path};
+use std::path::{Component, Path, PathBuf};
 
 use raylib::prelude::*;
 
@@ -34,13 +34,21 @@ const TEXT_PADDING : i32 = 10;
 
 const BACKGROUND_COLOR                      : Color = Color::LIGHTGRAY;
 
+
+const PALLET_SELECTED_COLOR : Color = Color::RED;
+const PALLET_DEFAULT_COLOR  : Color = Color::BLUE;
+
 const HIGHLIGHT_COLOR                       : Color = Color::ORANGE;
 
 const GRID_START_POSITION   : Vector2 = Vector2::new(100.0, 100.0);
+const PALLET_START_POSITION : Vector2 = Vector2::new(10.0, 10.0);
+
+pub struct ImageContainer {
+    pub image: Image,
+    pub texture: Option<Texture2D>,
+}
 
 // these thing have to go together, so why not make it official?
-// TODO, replace
-#[allow(dead_code)]
 struct GridHandler {
     icon_server: MyIconServer<ImageContainer>,
     grid: TileGrid<String>,
@@ -50,9 +58,14 @@ struct GridHandler {
 fn main() {
     let assets = get_images_from_path(Path::new(PATH));
 
-    let mut icon_server = MyIconServer::new(assets);
+    let mut grid_handler = GridHandler {
+        icon_server: MyIconServer::new(assets),
+        grid: TileGrid::new(4, 6),
+    };
 
-    let mut grid = TileGrid::new(4, 6);
+    // let mut icon_server = MyIconServer::new(assets);
+
+    // let mut grid = TileGrid::new(4, 6);
 
     let (mut rl, thread) = raylib::init()
         .size(WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -77,7 +90,7 @@ fn main() {
             if rl.is_key_pressed(KeyboardKey::KEY_P) {
                 println!("Saving Grid!"); // TODO: draw something to the screen
 
-                let json_string = grid.to_json().to_string();
+                let json_string = grid_handler.grid.to_json().to_string();
                 let mut output = fs::File::create(QUICK_SAVE_FILE).expect("File was created");
                 write!(output, "{}", json_string).expect("Write to file");
             }
@@ -88,7 +101,7 @@ fn main() {
                     let mut buffer = String::new();
                     input.read_to_string(&mut buffer).expect("Read to buffer");
         
-                    grid = TileGrid::from_json(&json::parse(&buffer).unwrap()).expect("Grid loaded");
+                    grid_handler.grid = TileGrid::from_json(&json::parse(&buffer).unwrap()).expect("Grid loaded");
                 } else {
                     println!("No quick save file");
                 };
@@ -96,6 +109,7 @@ fn main() {
         }
 
         { // Grid Resizing
+            let grid = &mut grid_handler.grid;
             if rl.is_key_pressed(KeyboardKey::KEY_S) {                    grid.resize(grid.rows + 1, grid.cols    )  }
             if rl.is_key_pressed(KeyboardKey::KEY_W) { if grid.rows > 1 { grid.resize(grid.rows - 1, grid.cols    ) }}
             if rl.is_key_pressed(KeyboardKey::KEY_D) {                    grid.resize(grid.rows    , grid.cols + 1)  }
@@ -103,6 +117,7 @@ fn main() {
         }
 
         { // Selection cycling
+            let icon_server = &mut grid_handler.icon_server;
             if rl.is_key_pressed(KeyboardKey::KEY_E) { icon_server.cycle_selected( 1) }
             if rl.is_key_pressed(KeyboardKey::KEY_Q) { icon_server.cycle_selected(-1) }
             if rl.is_key_pressed(KeyboardKey::KEY_X) { icon_server.cycle_default ( 1) }
@@ -133,19 +148,11 @@ fn main() {
         
         // don't need to check if open, dose that automatically 
         let new_image = file_dialog_context.update(&mouse_context, &mut rl);
-        if let Some(path) = new_image {
-            // TODO: this should be simpler
-            if path.is_dir() {
-                icon_server.load_icons(&mut get_images_from_path(&path));
-            } else {
-                icon_server.load_icon(get_image_from_path(&path));
-            }
-            textures_dirty = true; // Remember to call when adding images
-        }
+        textures_dirty |= grid_handler.add_images(new_image);
 
         /* -------------------- LOAD TEXTURES -------------------- */
         if textures_dirty {
-            for (_, image_container) in icon_server.assets.iter_mut() {
+            for (_, image_container) in grid_handler.icon_server.assets.iter_mut() {
                 let mut image = image_container.image.clone();
                 image.resize(SQUARE_SIZE_F as i32, SQUARE_SIZE_F as i32);
                 
@@ -157,26 +164,22 @@ fn main() {
         }
         /* -------------------- LOAD TEXTURES -------------------- */
 
-        icon_server.update_pallet(&mouse_context);
-
-        update_grid_mouse_events(&mut grid, &mut icon_server, &mouse_context);
+        grid_handler.update(&mouse_context);
         
         /* -------------------- DRAWING -------------------- */
         let mut d = rl.begin_drawing(&thread);
 
-
         d.clear_background(BACKGROUND_COLOR);
+
 
         // TODO: Move the grid out of the way
         /* -------------------- DRAW GRID -------------------- */
-        let grid_panel = grid_to_pallet(&grid, &icon_server);
-
+        let grid_panel = grid_handler.to_grid_panel();
         grid_panel.draw_panel(&mut d, &mouse_context);
         /* -------------------- DRAW GRID -------------------- */
 
         /* -------------------- DRAW PALLET -------------------- */
-        let pallet_panel = icon_server.to_pallet_panel();
-
+        let pallet_panel = grid_handler.to_pallet_panel();
         pallet_panel.draw_panel(&mut d, &mouse_context);
         /* -------------------- DRAW PALLET -------------------- */
 
@@ -229,49 +232,106 @@ fn get_images_from_path(path: &Path) -> Vec<(String, ImageContainer)> {
         .collect()
 }
 
-fn grid_to_pallet<'a>(grid: &TileGrid<String>, icon_server: &'a MyIconServer<ImageContainer>) -> GridPanel<&'a Texture2D> {
-    let mut panel = GridPanel::new_custom(
-        GRID_START_POSITION,
-        true, grid.cols,
-        64, 64, 10,
-        Some(Color::ORANGE), None
-    );
+impl<'a> GridHandler {
+    fn to_grid_panel(&'a self) -> GridPanel<&'a Texture2D> {
+        let (rows, cols) = self.grid.size();
+        
+        let mut panel = GridPanel::new_custom(
+            GRID_START_POSITION,
+            true, cols,
+            64, 64, 10,
+            Some(Color::ORANGE), None
+        );
 
-    for i in 0..grid.rows*grid.cols {
-        let image_container = if let Some(name) = grid.get_from_index(i) {
-            icon_server.get_by_name(name).expect("Name exist in icon server")
+        for i in 0..rows*cols {
+            let image_container = if let Some(name) = self.grid.get_from_index(i) {
+                self.icon_server.get_by_name(name).expect("Name exist in icon server")
+            } else {
+                self.icon_server.get_default_handle()
+            };
+    
+            let Some(texture) = image_container.texture.as_ref() else {
+                panel.add_none();
+                continue;
+            };
+    
+            panel.add(texture);
+        }
+    
+        return panel;
+    }
+
+    fn to_pallet_panel(&self) -> GridPanel<&Texture2D> {
+        let mut panel = GridPanel::new_custom(
+            PALLET_START_POSITION,
+            true, 3,
+            SQUARE_SIZE, SQUARE_SIZE, SQUARE_SPACING,
+            Some(HIGHLIGHT_COLOR), None
+        );
+
+        for (name, image_container) in self.icon_server.assets.iter() {
+            let Some(texture) = image_container.texture.as_ref() else {
+                panel.add_none();
+                continue;
+            };
+
+            let mut highlights = vec![];
+
+            if self.icon_server.get_default_name()  == name { highlights.push(PALLET_DEFAULT_COLOR) }
+            if self.icon_server.get_selected_name() == name { highlights.push(PALLET_SELECTED_COLOR) }
+
+            panel.add_with_highlight(texture, &highlights);
+        }
+
+        return panel;
+    }
+
+    fn update(&mut self, mouse_context: &MouseContext) {
+        self.update_pallet(mouse_context);
+        self.update_grid(mouse_context);
+    }
+
+    fn update_grid(&mut self, mouse_context: &MouseContext) {
+        let grid_pallet = self.to_grid_panel();
+
+        let id = grid_pallet.get_hovered_id(mouse_context);
+
+        let Some(id) = id else { return; };
+
+        let pos = index_to_pos(id, self.grid.size());
+
+        if mouse_context.mouse_left_pressed {
+            self.grid.set(pos, Some(self.icon_server.get_selected_name().to_string()));
+        }
+        if mouse_context.mouse_right_pressed {
+            self.grid.set(pos, None);
+        }
+    }
+
+    pub fn update_pallet(&mut self, mouse_context: &MouseContext) {
+        let pallet_panel = self.to_pallet_panel();
+
+        let Some(id) = pallet_panel.get_hovered_id(&mouse_context) else { return; };
+
+        let name = self.icon_server.assets[id].0.clone();
+
+        if mouse_context.mouse_left_pressed {
+            self.icon_server.set_selected_by_name(&name);
+        }
+        if mouse_context.mouse_right_pressed {
+            self.icon_server.set_default_by_name(&name);
+        }
+    }
+
+    // returns textures_dirty
+    fn add_images(&mut self, new_image: Option<PathBuf>) -> bool {
+        let Some(path) = new_image else { return false; };
+            // TODO: this should be simpler
+        if path.is_dir() {
+            self.icon_server.load_icons(&mut get_images_from_path(&path));
         } else {
-            icon_server.get_default_handle()
-        };
-
-        let Some(texture) = image_container.texture.as_ref() else {
-            panel.add_none();
-            continue;
-        };
-
-        panel.add(texture);
-    }
-
-    return panel;
-}
-
-fn update_grid_mouse_events(
-    grid: &mut TileGrid<String>,
-    icon_server: &MyIconServer<ImageContainer>,
-    mouse_context: &MouseContext,
-) {
-    let grid_pallet = grid_to_pallet(grid, icon_server);
-
-    let id = grid_pallet.get_hovered_id(mouse_context);
-
-    let Some(id) = id else { return; };
-
-    let pos = index_to_pos(id, grid.size());
-
-    if mouse_context.mouse_left_pressed {
-        grid.set(pos, Some(icon_server.get_selected_name().to_string()));
-    }
-    if mouse_context.mouse_right_pressed {
-        grid.set(pos, None);
+            self.icon_server.load_icon(get_image_from_path(&path));
+        }
+        return true; // textures_dirty = true; // Remember to call when adding images
     }
 }
